6e959b19747d2888c7406f5ee1cead0f0b6c53a0
Yogesh K <armsvcmode@gmail.com>
2022-06-10 12:30:18 +0530

    First Working version

--- packages/frontend/src/pages/test-doctor-analytics.tsx
+++ packages/frontend/src/pages/test-doctor-analytics.tsx
@@ -15,7 +15,7 @@ import {
 import { utils } from 'ethers'
 import ABIs from '../lib/hardhat_contracts.json'
 import covalent from './test-covalent'
-
+import { useEthers } from '@usedapp/core'
 export default function DoctorAnalyticsTest() {
     const [data, setData] = useState(null)
     const [decodedEventGrantAccess, setDecodedEventGrantAccess] = useState([])
@@ -23,6 +23,8 @@ export default function DoctorAnalyticsTest() {
     const [selectedEvent,setSelectedEvent] = useState('')
     const [startingBlock, setStartingBlock] = useState('26639009')
     const [endingBlock, setEndingBlock] = useState('latest')
+    const {library} = useEthers()
+
 
     useEffect(() => {
         initEventData()
@@ -54,6 +56,7 @@ export default function DoctorAnalyticsTest() {
         })
         setEventData(arr)
         console.log("Inside initEventData:", arr)
+
        }
 
     async function downloadEventLog() {
@@ -70,53 +73,62 @@ export default function DoctorAnalyticsTest() {
                         return d
                     })
                 console.log(d)
-                getDecodedGrantAccess(d)
+                   getDecodedGrantAccess(d)
             } catch (e) {
                 alert(e)
             }
 
     }
 
-    function getDecodedGrantAccess(d) {
+    async function getDecodedGrantAccess(d) {
         let arr = []
+        let signerAddr = await getSignerAddress()
         d && d.data.items.map(
-            (item) => {
+               (item)  =>  {
                 let index = 0;
-                eventData.map((item, i) => {
-                    if (item.name == 'GrantAccess') {
+                    eventData.map((item, i) => {
+                       if (item.name == 'GrantAccess') {
                         index = i
-                    }
-                });
-                console.log("For grant access", eventData[index].eventTopicNameEncoded)
+                       }
+                    });
                 console.log(index, String(item.raw_log_topics[0]), eventData[index].eventTopicNameEncoded)
                 if (String(item.raw_log_topics[0]) == eventData[index].eventTopicNameEncoded) {
                     const addr = utils.defaultAbiCoder.decode(['address'], item.raw_log_topics[1])
                     const decoded = utils.defaultAbiCoder.decode(['uint256', 'uint256', 'uint256[]'], item.raw_log_data)
                     const patientAddr = item.sender_address
-                    console.log("patientAddr:", item.sender_address)
+                    //console.log("patientAddr:", item.sender_address)
 
                     // push only those contents belonging to the signed in address (i.e doctors wallet addr)
                     // The grantAddress will be the 'grant to user' = doctors wallet addr
-                    
-                    let contractEvent = {
-                        topic: String(item.raw_log_topics[0]),
-                        grantAddress: String(addr),
-                        start: String(decoded[0]),
-                        end: String(decoded[1]),
-                        documentTypes: String(decoded[2]),
-                        patientAddress:String(patientAddr)
+                    // We check if the recd address is same as signed in address
+                    if (signerAddr == String(addr))
+                    {
+                        let contractEvent = {
+                            topic: String(item.raw_log_topics[0]),
+                            grantAddress: String(addr),
+                            start: String(decoded[0]),
+                            end: String(decoded[1]),
+                            documentTypes: String(decoded[2]),
+                            patientAddress:String(patientAddr)
+                        }
+                        arr.push(contractEvent)
                     }
-                    arr.push(contractEvent)
-                    // console.log(decoded)
                 }
             }
         )
         setDecodedEventGrantAccess(arr)
     }
 
-    function ViewDocTimelines(){
+   
+
+    async function ViewDocTimelines(){
         downloadEventLog()
         setSelectedEvent('GrantAccess')
+    }
+
+    async function getSignerAddress(){
+        const signer = library?.getSigner()
+        return await signer.getAddress()
     }
 
 
@@ -127,16 +139,16 @@ export default function DoctorAnalyticsTest() {
                 Doctor Dashboard
             </Center>
         </Heading>
+        <Box>
             <Center>
                 <Button variant = 'solid' m='10' p='5' onClick={ViewDocTimelines}> View Document Timelines </Button>
             </Center>
-        <Box>
             {
-                (selectedEvent == 'GrantAccess') 
+                (selectedEvent == 'GrantAccess')
                 && (
                             <TableContainer>
                                 <Table variant='striped' colorScheme='teal'>
-                                    <TableCaption placement='top'>Patient Document Timelines</TableCaption>
+                                    <TableCaption placement='top' fontWeight= 'bold'>Patient Document Timelines</TableCaption>
                                     <Thead>
                                         <Tr>
                                             <Th>Patient Address</Th>
@@ -145,25 +157,25 @@ export default function DoctorAnalyticsTest() {
                                             <Th>Access End</Th>
                                         </Tr>
                                     </Thead>
-                               </Table>
+                                  </Table>
                             </TableContainer>
-
-                 )
+                   )
             }
         </Box>
         <Box>
             {
-                (decodedEventGrantAccess.map((item, i) => {
+                (selectedEvent == 'GrantAccess') 
+                && (decodedEventGrantAccess.map((item, i) => {
                                 return (< div key={i}>
                                         <Divider my='1'></Divider>
                                             <TableContainer>                                          
                                                 <Table variant='striped' colorScheme='teal'>
                                                     <Tbody>
                                                     <Tr>
-                                                        <Td overflowX='auto'>{item.patientAddress}</Td>
-                                                        <Td overflowX='auto'>{item.documentTypes}</Td>
-                                                        <Td isNumeric>{item.start}</Td>
-                                                        <Td isNumeric>{item.end}</Td>
+                                                        <Td >{item.patientAddress}</Td>
+                                                        <Td>{item.documentTypes}</Td>
+                                                        <Td >{item.start}</Td>
+                                                        <Td >{item.end}</Td>
                                                     </Tr>
                                                     </Tbody>
                                                 </Table> 
@@ -179,30 +191,3 @@ export default function DoctorAnalyticsTest() {
    )
 
 }
-
-
-/*
-
-                (selectedEvent == 'GrantAccess' && decodedEventGrantAccess) && decodedEventGrantAccess.map((item, i) => {
-                        return (< div key={i}>
-                                <Divider my='1'></Divider>
-                            <TableContainer>
-                                    <Table variant='striped' colorScheme='teal'>
-                                        <Tbody>
-                                        <Tr>
-                                            <Td>{item.patientAddress}</Td>
-                                            <Td>{item.documentTypes}</Td>
-                                            <Td>{item.start}</Td>
-                                            <Td isNumeric>{item.end}</Td>
-                                        </Tr>
-                                        </Tbody>
-                                    </Table>
-                             </TableContainer>
-                              </div>
-                )
-              })
-
-
-
-
-*/
